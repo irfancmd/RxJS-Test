f19d6ee3562b8100577aa5c3578b8f289d9f29a1
Chowdhury Mohammad Irfanuddin <irfanuddincmd@gmail.com>
2023-10-04 03:16:42 +0600

    Creating an observer manually.

--- Basics/main.ts
+++ Basics/main.ts
@@ -1,16 +1,26 @@
-import { Observer, from } from "rxjs";
+import { Observable, Observer, from } from "rxjs";
 
 /* Let' create a date source. It can be an from an api or a promise. But for simplicity,
    let's stick to arrays.
 */
 let numbers = [1, 5, 10];
 
-// Now, we're creating an Observable that keeps track of our data source for changes.
-let source = from(numbers);
+// Now, we'll create an observer manually using the low level "create" function
+const source = new Observable((observer) => {
+  for (const num of numbers) {
+    if (num === 5) {
+      /* If stop the observer like this, it will not read next values of the observable
+         and it will not call the complete method either.
+      */
+      observer.error("An error occurred.");
+    }
 
-/* Rather than creating classes, we can also pass observer functons direcly in the
-   subscribe method by putting them inside an object.
-*/
+    observer.next(num);
+  }
+
+  observer.complete();
+});
+
 source.subscribe({
   next: (value) => console.log(`Value: ${value}`),
   error: (error) => console.log(`Value: ${error}`),
