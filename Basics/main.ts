2af77b0f125dfadba8824eb570b3f940dd853af0
Chowdhury Mohammad Irfanuddin <irfanuddincmd@gmail.com>
2023-10-04 04:06:12 +0600

    Pipes and operators

--- Basics/main.ts
+++ Basics/main.ts
@@ -1,11 +1,10 @@
-import { Observable, Observer, from } from "rxjs";
+import { Observable, Observer, filter, from, map } from "rxjs";
 
 /* Let' create a date source. It can be an from an api or a promise. But for simplicity,
    let's stick to arrays.
 */
 let numbers = [1, 5, 10];
 
-// We will now look an example of an asynchronous Observable
 const source = new Observable((observer) => {
   let index = 0;
 
@@ -14,14 +13,16 @@ const source = new Observable((observer) => {
     index++;
 
     if (index < numbers.length) {
-      setTimeout(produceValue, 2000);
+      setTimeout(produceValue, 1000);
     } else {
       observer.complete();
     }
   };
 
   produceValue();
-});
+})
+  .pipe(map((n: number) => n * 2)) // We're piping the observable data through the map operator
+  .pipe(filter((n) => n > 4)); // Then, we're piping the observable data through the filter operator
 
 source.subscribe({
   next: (value) => console.log(`Value: ${value}`),
